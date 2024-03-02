14e98a08e4e1217a06d659fccde1fafcf8c25e0d
jpfbastos <jpfbastos05@gmail.com>
2024-03-02 17:58:44 +0000

    Eval so far (slow decay into insanity)

---
+++ eval/src/Eval.scala
@@ -0,0 +1,306 @@
+import DreamBerd.Syntax.Syntax._
+import scala.collection.immutable.ListMap
+
+object Eval {
+
+  // ======================================================================
+  // Capture-avoiding substitution
+  // ======================================================================
+
+  val generator = SymGenerator()
+
+  object SubstExpr extends Substitutable[Expr] {
+    // swap y and z in e
+    def swap(e: Expr, y: Variable, z: Variable): Expr =
+      def go(e: Expr): Expr = e match {
+        // Value must be closed
+        case v: Value => v
+
+        case Unit => Unit
+
+        case Num(n)        => Num(n)
+        case Plus(e1, e2)  => Plus(go(e1), go(e2))
+        case Minus(e1, e2) => Minus(go(e1), go(e2))
+        case Times(e1, e2) => Times(go(e1), go(e2))
+
+        case Bool(b)               => Bool(b)
+        case Eq(e1, e2)            => Eq(go(e1), go(e2))
+        case IfThenElse(e, e1, e2) => IfThenElse(go(e), go(e1), go(e2))
+
+        case Str(s)         => Str(s)
+        case Length(e)      => Length(go(e))
+        case Index(e1, e2)  => Index(go(e1), go(e2))
+        case Concat(e1, e2) => Concat(go(e1), go(e2))
+
+        case Var(x)         => Var(swapVar(x, y, z))
+        case Let(x, e1, e2) => Let(swapVar(x, y, z), go(e1), go(e2))
+
+        case Pair(e1, e2) => Pair(go(e1), go(e2))
+        case First(e)     => First(go(e))
+        case Second(e)    => Second(go(e))
+
+        case Lambda(x, e)  => Lambda(swapVar(x, y, z), go(e))
+        case Apply(e1, e2) => Apply(go(e1), go(e2))
+        case Rec(f, x, e)  => Rec(swapVar(f, y, z), swapVar(x, y, z), go(e))
+
+        case Record(es) => Record(es.map((x, e) => (x, go(e))))
+        case Proj(e, l) => Proj(go(e), l)
+
+        case Ref(e)         => Ref(go(e))
+        case Deref(e)       => Deref(go(e))
+        case Assign(e1, e2) => Assign(go(e1), go(e2))
+
+        case Sequ(e1, e2) => Sequ(go(e1), go(e2))
+        case LetPair(x1, x2, e1, e2) =>
+          LetPair(swapVar(x1, y, z), swapVar(x2, y, z), go(e1), go(e2))
+        case LetFun(f, x, e1, e2) =>
+          LetFun(swapVar(f, y, z), swapVar(x, y, z), go(e1), go(e2))
+        case LetRec(f, x, e1, e2) =>
+          LetRec(swapVar(f, y, z), swapVar(x, y, z), go(e1), go(e2))
+        case LetRecord(xs, e1, e2) =>
+          LetRecord(xs.map((l, x) => (l, swapVar(x, y, z))), go(e1), go(e2))
+      }
+      go(e)
+
+      def apply(theta: Subst[Expr], e: Expr): Expr =
+        // BEGIN ANSWER
+        e match {
+          case Num(e)        => Num(e)
+          case Plus(t1, t2)  => Plus(apply(theta, t1), apply(theta, t2))
+          case Minus(t1, t2) => Minus(apply(theta, t1), apply(theta, t2))
+          case Times(t1, t2) => Times(apply(theta, t1), apply(theta, t2))
+
+          // Booleans
+          case Bool(b)    => Bool(b)
+          case Eq(t1, t2) => Eq(apply(theta, t1), apply(theta, t2))
+          case IfThenElse(t0, t1, t2) =>
+            IfThenElse(apply(theta, t0), apply(theta, t1), apply(theta, t2))
+
+          // Strings
+          case Str(s)         => Str(s)
+          case Length(t0)     => Length(apply(theta, t0))
+          case Index(t1, t2)  => Index(apply(theta, t1), apply(theta, t2))
+          case Concat(t1, t2) => Concat(apply(theta, t1), apply(theta, t2))
+
+          case Let(y, t1, t2) => {
+            val z = generator.genVar(y);
+            Let(z, apply(theta, t1), apply(theta, swap(t2, y, z)))
+          }
+
+          // Pairs
+          case Pair(t1, t2) => Pair(apply(theta, t1), apply(theta, t2))
+          case First(t0)    => First(apply(theta, t0))
+          case Second(t0)   => Second(apply(theta, t0))
+
+          // Functions
+          case Lambda(x, e) => {
+            val y = generator.genVar(x);
+            Lambda(y, apply(theta, swap(e, x, y)))
+          }
+          case Apply(t1, t2) => Apply(apply(theta, t1), apply(theta, t2))
+          case Rec(f, x, e) => {
+            val g = generator.genVar(f);
+            val y = generator.genVar(x);
+            Rec(g, y, apply(theta, swap(swap(e, f, g), x, y)))
+          }
+
+          // Syntactic sugar
+          case LetPair(y1, y2, t1, t2) => {
+            val y1z = generator.genVar(y1);
+            val y2z = generator.genVar(y2);
+            LetPair(
+              y1z,
+              y2z,
+              apply(theta, t1),
+              apply(theta, swap(swap(t2, y1z, y1), y2z, y2))
+            )
+          }
+
+          case LetFun(f, x, e1, e2) => {
+            val g = generator.genVar(f);
+            val y = generator.genVar(x);
+            LetFun(
+              g,
+              y,
+              apply(theta, swap(swap(e1, f, g), x, y)),
+              apply(theta, swap(e2, f, g))
+            )
+          }
+
+          case LetRec(f, x, e1, e2) => {
+            val g = generator.genVar(f);
+            val y = generator.genVar(x);
+            LetRec(
+              g,
+              y,
+              apply(theta, swap(swap(e1, f, g), x, y)),
+              apply(theta, swap(e2, f, g))
+            )
+          }
+
+          case Var(y) => theta.getOrElse(y, Var(y))
+
+          case Record(es) => Record(es.map((l, t) => (l, apply(theta, t))))
+          case Proj(t, l) => Proj(apply(theta, t), l)
+          case Ref(t)     => Ref(apply(theta, t))
+          case Deref(t)   => Deref(apply(theta, t))
+          case Assign(t1, t2) =>
+            Assign(apply(theta, t1), apply(theta, t2))
+          case Sequ(t1, t2) =>
+            Sequ(apply(theta, t1), apply(theta, t2))
+          case LetRecord(xs, t1, t2) => {
+            val ys = xs.map((l, x) => (l, generator.genVar(x)));
+            val theta1 = theta ++ xs.zip(ys).map((x, y) => (x._2, Var(y._2)));
+            LetRecord(
+              ys,
+              apply(theta, t1),
+              apply(theta1, t2)
+            )
+          }
+          case v: Value => v
+          case Unit     => Unit
+          // END ANSWER
+        }
+  }
+  import SubstExpr.{subst, ra2AppAssoc, ra2CompAssoc}
+
+  object Value {
+    // utility methods for operating on values
+    def add(v1: Value, v2: Value): Value = (v1, v2) match
+      case (NumV(v1), NumV(v2)) => NumV(v1 + v2)
+      case _ =>
+        sys.error("arguments to addition are non-numeric " + v1 + " " + v2)
+
+    def subtract(v1: Value, v2: Value): Value = (v1, v2) match
+      case (NumV(v1), NumV(v2)) => NumV(v1 - v2)
+      case _ => sys.error("arguments to subtraction are non-numeric")
+
+    def multiply(v1: Value, v2: Value): Value = (v1, v2) match
+      case (NumV(v1), NumV(v2)) => NumV(v1 * v2)
+      case _ => sys.error("arguments to multiplication are non-numeric")
+
+    def eq(v1: Value, v2: Value): Value = (v1, v2) match
+      case (NumV(v1), NumV(v2))       => BoolV(v1 == v2)
+      case (BoolV(v1), BoolV(v2))     => BoolV(v1 == v2)
+      case (StringV(v1), StringV(v2)) => BoolV(v1 == v2)
+      case _ => sys.error("arguments to = are not base types " + v1 + " " + v2)
+
+    def length(v: Value): Value = v match
+      case StringV(v1) => NumV(v1.length)
+      case _           => sys.error("argument to length is not a string")
+
+    def index(v1: Value, v2: Value): Value = (v1, v2) match
+      case (StringV(v1), NumV(v2)) => StringV(v1.charAt(v2).toString)
+      case _ => sys.error("arguments to index are not valid")
+
+    def concat(v1: Value, v2: Value): Value = (v1, v2) match
+      case (StringV(v1), StringV(v2)) => StringV(v1 ++ v2)
+      case _ => sys.error("arguments to concat are not strings")
+  }
+
+  // ======================================================================
+  // Evaluation
+  // ======================================================================
+
+  def eval(e: Expr): Value = e match {
+    // Value
+    case v: Value => v
+
+    // BEGIN ANSWER
+    case Plus(e1, e2)  => Value.add(eval(e1), eval(e2))
+    case Minus(e1, e2) => Value.subtract(eval(e1), eval(e2))
+    case Times(e1, e2) => Value.multiply(eval(e1), eval(e2))
+    case IfThenElse(e, e1, e2) =>
+      if eval(e) == BoolV(true) then eval(e1) else eval(e2)
+    case Eq(e1, e2)     => Value.eq(eval(e1), eval(e2))
+    case Length(e)      => Value.length(eval(e))
+    case Index(e1, e2)  => Value.index(eval(e1), eval(e2))
+    case Concat(e1, e2) => Value.concat(eval(e1), eval(e2))
+    case Apply(e1, e2) =>
+      val v1 = eval(e1)
+      val v2 = eval(e2)
+      v1 match {
+        case FunV(x, e)    => eval(subst(e, v2, x))
+        case RecV(f, x, e) => eval(subst(subst(e, v1, f), v2, x))
+        case _             => sys.error("eval: apply e1 not a function")
+      }
+    case Let(x, e1, e2) => eval(subst(e2, eval(e1), x))
+    case Pair(e1, e2)   => PairV(eval(e1), eval(e2))
+    case First(e) =>
+      eval(e) match {
+        case PairV(v1, v2) => v1
+        case _             => sys.error("eval: First e not a pair")
+      }
+    case Second(e) =>
+      eval(e) match {
+        case PairV(v1, v2) => v2
+        case _             => sys.error("eval: Second e not a pair")
+      }
+    case Record(es) => RecordV(es.map((l, e) => (l, eval(e))))
+    case Proj(e, l) =>
+      eval(e) match {
+        case RecordV(es) => es(l)
+        case _           => sys.error("eval: Proj e not a record")
+      }
+    case Ref(e) => Cell(RefCell(eval(e)))
+    case Deref(e) =>
+      eval(e) match {
+        case Cell(r) => r.get
+        case _       => sys.error("eval: deref not a cell")
+      }
+    case Assign(e1, e2) =>
+      eval(e1) match {
+        case Cell(r) => r.set(eval(e2)); UnitV
+        case _       => sys.error("eval: assign not a cell")
+      }
+    case Num(n)       => NumV(n)
+    case Bool(b)      => BoolV(b)
+    case Str(s)       => StringV(s)
+    case Var(x)       => sys.error("eval: free variable: " + x)
+    case Lambda(x, e) => FunV(x, e)
+    case Rec(f, x, e) => RecV(f, x, e)
+    case Unit         => UnitV
+    case _            => sys.error("eval: should have been desugared " + e)
+    // END ANSWER
+  }
+
+  
+    type Subst[A] = ListMap[Variable, A]
+    object Subst {
+    def empty[A]: Subst[A] = ListMap.empty[Variable, A]
+    def apply[A](pairs: (Variable, A)*): Subst[A] = ListMap(pairs: _*)
+  }
+  // the empty substitution ι
+  def iota[A]: Subst[A] = Subst.empty
+
+  trait Applicable[A, B] {
+    // apply the substitution theta to t
+    def apply(theta: Subst[A], t: B): B
+
+    // Scala magic to define the right-associative infix operator +:
+    class AppAssoc(x: B) {
+      def +:(theta: Subst[A]): B = apply(theta, x)
+    }
+    implicit def ra2AppAssoc(x: B): AppAssoc = new AppAssoc(x)
+  }
+
+  // a trait for substitutable things, e.g., expressions and types
+  trait Substitutable[A] extends Applicable[A, A] {
+    // swap y and z in t
+    def swap(t: A, y: Variable, z: Variable): A
+
+    // subst x in t1 with t2, i.e., t1[t2/x]
+    def subst(t1: A, t2: A, x: Variable): A =
+      apply(ListMap(x -> t2), t1)
+
+    // compose two subsitutions, i.e., s1 ∘ s2
+    def comp(s1: Subst[A], s2: Subst[A]): Subst[A] =
+      s1 ++ s2.map((x: Variable, t: A) => (x, apply(s1, t)))
+
+    // Scala magic to define the right-associative infix operator *:
+    class CompAssoc(y: Subst[A]) {
+      def *:(x: Subst[A]): Subst[A] = comp(x, y)
+    }
+    implicit def ra2CompAssoc(x: Subst[A]): CompAssoc = new CompAssoc(x)
+  }
+}
---
+++ eval/src/Syntax.scala
@@ -0,0 +1,182 @@
+package DreamBerd.Syntax
+
+import java.security.Identity
+import scala.language.implicitConversions
+import scala.collection.immutable.ListMap
+import scala.collection.Seq
+
+object Syntax {
+  type Variable = String
+  type Env[A] = ListMap[Variable, A]
+  type Label = String
+  type Field[A] = ListMap[Label, A]
+  type Clauses = ListMap[(Label, Variable), Expr]
+
+  class RefCell[A](val x: A) {
+    private var a = x
+    def get = a
+    def set(y: A) = a = y
+  }
+
+  // ======================================================================
+  // Expressions
+  // ======================================================================
+  sealed abstract class Expr
+
+  // Unit
+  case object Unit extends Expr
+
+  // Arithmetic expressions
+  case class Num(n: Integer) extends Expr
+  case class Plus(e1: Expr, e2: Expr) extends Expr
+  case class Minus(e1: Expr, e2: Expr) extends Expr
+  case class Times(e1: Expr, e2: Expr) extends Expr
+  case class Divide(e1: Expr, e2: Expr) extends Expr
+
+  // Booleans
+  case class Bool(b: Boolean) extends Expr
+  case class Eq(e1: Expr, e2: Expr) extends Expr
+  case class IfThenElse(e: Expr, e1: Expr, e2: Expr) extends Expr
+
+  // Strings
+  case class Str(s: String) extends Expr
+  case class Length(e: Expr) extends Expr
+  case class Index(e1: Expr, e2: Expr) extends Expr
+  case class Concat(e1: Expr, e2: Expr) extends Expr
+
+  // Variables and let-binding
+  case class Var(x: Variable) extends Expr
+  case class Let(x: Variable, e1: Expr, e2: Expr) extends Expr
+
+  // Functions
+  case class Lambda(x: Variable, e: Expr) extends Expr
+  case class Apply(e1: Expr, e2: Expr) extends Expr
+  case class Rec(f: Variable, x: Variable, e: Expr) extends Expr
+
+  // Pairing
+  case class Pair(e1: Expr, e2: Expr) extends Expr
+  case class First(e: Expr) extends Expr
+  case class Second(e: Expr) extends Expr
+
+  // Records
+  case class Record(es: Field[Expr]) extends Expr
+  case class Proj(e: Expr, l: Label) extends Expr
+
+  // References
+  case class Ref(e: Expr) extends Expr
+  case class Deref(e: Expr) extends Expr
+  case class Assign(e1: Expr, e2: Expr) extends Expr
+
+  // Syntactic sugars
+  case class LetPair(x: Variable, y: Variable, e1: Expr, e2: Expr) extends Expr
+  case class LetFun(f: Variable, arg: Variable, e1: Expr, e2: Expr) extends Expr
+  case class LetRec(f: Variable, arg: Variable, e1: Expr, e2: Expr) extends Expr
+  case class LetRecord(xs: Field[Variable], e1: Expr, e2: Expr) extends Expr
+  case class Sequ(e1: Expr, e2: Expr) extends Expr
+
+  // Values
+  abstract class Value extends Expr
+  case object UnitV extends Value
+  case class NumV(n: Integer) extends Value
+  case class BoolV(b: Boolean) extends Value
+  case class StringV(s: String) extends Value
+  case class PairV(v1: Value, v2: Value) extends Value
+  case class RecordV(vs: Field[Value]) extends Value
+  case class Cell(r: RefCell[Value]) extends Value
+  // NOTE: Cell is not a syntactic value, thus cannot be generalised
+
+  case class FunV(x: Variable, e: Expr) extends Value
+  case class RecV(f: Variable, x: Variable, e: Expr) extends Value
+
+  // ======================================================================
+  // Types
+  // ======================================================================
+  sealed abstract class Type
+
+  // Monomorphic types
+  case object TyUnit extends Type
+  case object TyInt extends Type
+  case object TyBool extends Type
+  case object TyString extends Type
+  case class TyPair(ty1: Type, ty2: Type) extends Type
+  case class TyFun(ty1: Type, ty2: Type) extends Type
+  case class TyRef(ty: Type) extends Type
+  case class TyRecord(tys: Field[Type]) extends Type
+  case class TyVar(x: Variable) extends Type
+
+  // Type schemes
+  case class TyForall(a: Variable, ty: Type) extends Type
+
+  // ======================================================================
+  // Substitutions
+  // ======================================================================
+
+  // a class for generating fresh variables
+  class SymGenerator {
+    private var id = 0
+    // generate a fresh variable from an existing variable
+    def genVar(s: Variable): Variable = {
+      val fresh_s = s + "_" + id
+      id = id + 1
+      fresh_s
+    }
+    // generate a fresh variable from nothing
+    def freshVar(): Variable = {
+      val fresh_s = "$" + id
+      id = id + 1
+      fresh_s
+    }
+  }
+
+  // swap y and z in x
+  def swapVar(x: Variable, y: Variable, z: Variable): Variable =
+    if x == y then z else if x == z then y else x
+
+  // a substitution is defined as a map
+  // all substitutions in Subst[A] are done simultaneously
+  type Subst[A] = ListMap[Variable, A]
+  object Subst {
+    def empty[A]: Subst[A] = ListMap.empty[Variable, A]
+    def apply[A](pairs: (Variable, A)*): Subst[A] = ListMap(pairs: _*)
+  }
+  // the empty substitution ι
+  def iota[A]: Subst[A] = Subst.empty
+
+  trait Applicable[A, B] {
+    // apply the substitution theta to t
+    def apply(theta: Subst[A], t: B): B
+
+    // Scala magic to define the right-associative infix operator +:
+    class AppAssoc(x: B) {
+      def +:(theta: Subst[A]): B = apply(theta, x)
+    }
+    implicit def ra2AppAssoc(x: B): AppAssoc = new AppAssoc(x)
+  }
+
+  // a trait for substitutable things, e.g., expressions and types
+  trait Substitutable[A] extends Applicable[A, A] {
+    // swap y and z in t
+    def swap(t: A, y: Variable, z: Variable): A
+
+    // subst x in t1 with t2, i.e., t1[t2/x]
+    def subst(t1: A, t2: A, x: Variable): A =
+      apply(ListMap(x -> t2), t1)
+
+    // compose two subsitutions, i.e., s1 ∘ s2
+    def comp(s1: Subst[A], s2: Subst[A]): Subst[A] =
+      s1 ++ s2.map((x: Variable, t: A) => (x, apply(s1, t)))
+
+    // Scala magic to define the right-associative infix operator *:
+    class CompAssoc(y: Subst[A]) {
+      def *:(x: Subst[A]): Subst[A] = comp(x, y)
+    }
+    implicit def ra2CompAssoc(x: Subst[A]): CompAssoc = new CompAssoc(x)
+  }
+
+  // Scala magic to define the right-associative infix operator <->
+  // precedence: <-> < +: < *:
+  class UnifyAssoc[A, B](x: A) {
+    def <->(y: B): (A, B) = (x, y)
+  }
+  implicit def ra2UnifyAssoc[A, B](x: A): UnifyAssoc[A, B] = new UnifyAssoc(x)
+}
