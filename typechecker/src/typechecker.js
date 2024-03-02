5a09ac5727331ff86e189a58c16a1cd94ce50559
DylanCavers <s2179041@ed.ac.uk>
2024-03-02 17:28:40 +0000

    relative filepaths from cli added

--- typechecker/src/typechecker.js
+++ typechecker/src/typechecker.js
@@ -1,4 +1,6 @@
 import MistralClient from '@mistralai/mistralai';
+import * as fs from 'fs';
+import * as path from 'path';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
@@ -6,15 +8,17 @@ async function typecheck(program, spec, client) {
   let message = "The spec of the programming language is:\n" + spec + " The program is as follows:\n " + program + " \n Is the program correct given the spec? Give a one word answer." 
 
   const chatResponse = await client.chat({
-      model: 'mistral-tiny',
-      messages: [{role: 'user', content: message}],
-    });
+    model: 'mistral-tiny',
+    messages: [{role: 'user', content: message}],
+  });
   
+  console.log(chatResponse.choices[0].message.content);
+
   let textRepsonse = chatResponse.choices[0].message.content;
   textRepsonse = textRepsonse.split(' ');
   const result = textRepsonse[0].split('.')[0];
 
-  console.log('Chat:', result);
+  console.log('Result: ', result);
 
   if (result.toLowerCase() === 'yes') {
     process.exit(0);
@@ -30,11 +34,13 @@ async function doesHalt(program, spec, client) {
     messages: [{role: 'user', content: message}],
   });
 
+  console.log(chatResponse.choices[0].message.content);
+
   let textRepsonse = chatResponse.choices[0].message.content;
   textRepsonse = textRepsonse.split(' ');
   const result = textRepsonse[0].split('.')[0];
 
-  console.log('Chat:', result);
+  console.log('Result: ', result);
 
   if (result.toLowerCase() === 'no') {
     process.exit(0);
@@ -43,6 +49,25 @@ async function doesHalt(program, spec, client) {
   }
 }
 
+// check args
+if (process.argv.length < 4) {
+  console.log('Needs more args, usage: typechecker <program> <spec>');
+  process.exit(1);
+}
+
+// process filepaths
+const programFile = process.argv[2];
+const specFile = process.argv[3];
+const absProgramFile = path.resolve(process.cwd(), programFile);
+const absSpecFile = path.resolve(process.cwd(), specFile);
+
+// parse args into strings
+const program = fs.readFileSync(absProgramFile).toString('utf-8');
+const spec = fs.readFileSync(absSpecFile).toString('utf-8');
+
+// create client
 const apiKey = process.env.MISTRAL_API_KEY || 'your_api_key';
 const client = new MistralClient(apiKey);
-typecheck('a', 'all lines must end with a ;', client);
+
+// typecheck
+typecheck(program, spec, client);
